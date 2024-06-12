a3b6ee5aaea26be6b8bb6387582f4a9f0f7c86c1
AlexSanti33 <santiago.jav27@gmail.com>
2024-06-11 21:26:38 -0600

    Se dockeriza y se agrega @Enable Discovery Cliente por que si no, en el contenedor no funciona

--- src/main/java/com/portafolio/microservicios/companies_crud_fallback/CompaniesCrudFallbackApplication.java
+++ src/main/java/com/portafolio/microservicios/companies_crud_fallback/CompaniesCrudFallbackApplication.java
@@ -2,8 +2,10 @@ package com.portafolio.microservicios.companies_crud_fallback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CompaniesCrudFallbackApplication {
 
 	public static void main(String[] args) {
